23c39119ae9e8454a94932c26369117f8159a2fd
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-03-03 08:57:53 +0200

    Add missing import

--- ckanext/statistics/translations.py
+++ ckanext/statistics/translations.py
@@ -1,6 +1,6 @@
 def _translations():
     """ Does nothing but hints message extractor to extract missing strings. """
-
+    from ckan.plugins.toolkit import _
     _('This')
     _('Last')
     _('Modified')
